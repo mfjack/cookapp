a27911d698f59c4f11d9c4e771c67099e7bf513c
mfjack <ferreira.marlon@live.com>
2024-04-17 16:06:57 -0300

    feat: add ingredient service

--- cookapp/src/services/ingredientsService.ts
+++ cookapp/src/services/ingredientsService.ts
@@ -1,7 +1,30 @@
 import { supabase } from './supabase';
 
-export const findAll = async () => {
+async function findByIds(ids: string[]) {
+	const { data } = await supabase
+		.from('ingredients')
+		.select()
+		.in('id', ids)
+		.order('name')
+		.returns<IngredientResponse[]>();
+
+	return data ?? [];
+}
+
+async function findByRecipeId(id: string) {
+	const { data } = await supabase
+		.from('recipes_ingredients')
+		.select('ingredients (id, name, image)')
+		.eq('recipe_id', id)
+		.returns<{ ingredients: IngredientResponse }[]>();
+
+	return data ? data.map(item => item.ingredients) : [];
+}
+
+async function findAll() {
 	const { data } = await supabase.from('ingredients').select().order('name').returns<IngredientResponse[]>();
 
 	return data ?? [];
-};
+}
+
+export { findAll, findByIds, findByRecipeId };
